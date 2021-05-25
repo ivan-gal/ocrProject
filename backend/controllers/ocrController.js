9b24736ac6fa36be55265c92dcb7b82ca94ad874
Ivan Fernandez <ivan.ferndz94@gmail.com>
2021-05-26 00:18:21 +0200

    added ocr

--- backend/controllers/ocrController.js
+++ backend/controllers/ocrController.js
@@ -1,4 +1,25 @@
-const getFile = () => {};
+const vision = require('@google-cloud/vision');
+
+const client = new vision.ImageAnnotatorClient({
+    keyFilename: '../key.json',
+});
+
+const getFile = async (req, res, next) => {
+    const { files } = req;
+
+    const textFromDoc = await getGoogleVision(files[0]);
+};
+
+const getGoogleVision = async (file, next) => {
+    try {
+        const [result] = await client.textDetection(file);
+        const detection = result.textAnnotations;
+        const { description } = detection[0];
+        return description;
+    } catch (err) {
+        next(error);
+    }
+};
 
 module.exports = {
     getFile,
