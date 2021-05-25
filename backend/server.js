11c3e7aa69c249e3d1e17977b096143d652830d5
Ivan Fernandez <ivan.ferndz94@gmail.com>
2021-05-25 23:50:23 +0200

    Creating the app and server files

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const path = require('path');
+
+//Initilizing the app
+
+const staticPath = path.resolve(__dirname, 'static');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(staticPath));
+
+app.post('api/uploadDoc');
+
+app.use(async (err, req, res, next) => {
+    const status = err.isJoi ? 400 : err.code || 500;
+    res.status(status);
+    res.send({ resultado: 'ERROR', error: err.message });
+});
